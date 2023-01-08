dfa0bac40e97c2abd0f3f847c491f9936c1f6614
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2023-01-08 13:29:02 +0500

    Remove active storage cash

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Post.create(title: '3birthday');
+Post.create(title: '1');
